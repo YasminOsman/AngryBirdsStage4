205bbb661f3cb73742887e8f1a1a19c925eb301a
Yasmin Osman <singsonger101@gmail.com>
2020-10-18 15:47:32 -0400

    SlingShot

--- sketch.js
+++ sketch.js
@@ -6,7 +6,8 @@ const Constraint = Matter.Constraint;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
-var log6;
+//var log6;
+var slingshot;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -38,8 +39,8 @@ function setup(){
 
     bird = new Bird(100,100);
 
-    log6 = new Log(230,180,80,PI/2);
-    chain = new Chain(bird.body, log6.body);
+    //log6 = new Log(230,180,80,PI/2);
+    slingshot = new SlingShot(bird.body, {x: 200, y:100});
 }
 
 function draw(){
@@ -65,6 +66,14 @@ function draw(){
 
     bird.display();
     platform.display();
-    log6.display();
-    chain.display();
+    //log6.display();
+    slingshot.display();
+}
+
+function mouseDragged() {
+    Matter.Body.setPosition(bird.body, {x:mouseX,y:mouseY});
+}
+
+function mouseReleased() {
+    slingshot.fly();
 }
